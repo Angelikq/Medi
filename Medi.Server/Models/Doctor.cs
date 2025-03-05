f227e88305d82bf3349ccb7d04b3d6b54258f6b8
pawel <pawel.morawiak@studenci.collegiumwitelona.pl>
2025-03-05 18:35:52 +0100

    test

---
+++ Medi.Server/Models/Doctor.cs
@@ -0,0 +1,11 @@
+namespace Medi.Server.Models
+{
+    public class Doctor
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Email { get; set; }
+        public string Role { get; set; }
+    }
+}
---
+++ Medi.Server/Models/Patient.cs
@@ -0,0 +1,11 @@
+namespace Medi.Server.Models
+{
+    public class Patient
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Email { get; set; }
+        public string Role { get; set; }
+    }
+}
